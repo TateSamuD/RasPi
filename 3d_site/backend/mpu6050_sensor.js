55a2e37c8edb103a2d55a618cbc120d1485bfe1a
Tatenda Samudzi <tatenda.samudzi@gmail.com>
2023-12-23 18:24:49 -0600

    2 js files and 1 html added

--- 3d_site/backend/mpu6050_sensor.js
+++ 3d_site/backend/mpu6050_sensor.js
@@ -1,12 +1,21 @@
-const MPU6050 = require('mpu6050-gyro')
-const i2c = require('i2c-bus');
+const MPU6050 = require("mpu6050-gyro");
+const i2c = require("i2c-bus");
 
 const i2cBus = i2c.openSync(1);
 const mpu6050 = MPU6050(i2cBus, address);
 
-function initMPU6050(){
-    mpu6050.initialize();
-    return mpu6050;
+function initMPU6050() {
+  mpu6050.initialize();
+  return mpu6050;
 }
 
-function getMotionData(mpu)
+function getMotionData(mpu) {
+  const motion = mpu6050.get_gyro_xyz();
+  return {
+    pitch: motion.x,
+    roll: motion.y,
+    yaw: motion.z,
+  };
+}
+
+module.exports = { initMPU6050, getMotionData };
---
+++ 3d_site/backend/public/index.js
@@ -0,0 +1,36 @@
+const socket = io();
+
+socket.on("motionData", (motionData) => {
+  updateOjectRotation(motionData.pitch, motionData.roll, motionData.yaw);
+});
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function updateOjectRotation(pitch, roll, yaw) {
+  cube.rotation.x = pitch;
+  cube.rotation.y = roll;
+  cube.rotation.z = yaw;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
